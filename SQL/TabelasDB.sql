6719a9b5d96eef71bb7a634cd0e43c826d5f0637
Guilherme Fogo <gui.fogo17@gmail.com>
2020-08-17 15:22:35 -0300

    Passando Arquivo sql de criação das tabelas no banco

---
+++ SQL/TabelasDB.sql
@@ -0,0 +1,38 @@
+Create database teste;
+
+create table Endereco(
+	id_endereco int IDENTITY(1,1),
+	rua Nvarchar(45),
+	CEP NVarchar(45),
+	estado NVarchar(20),
+	cidade NVarchar(45),
+	complemento NVarchar(45),
+
+	constraint pk_endereco primary Key(id_endereco)
+);
+
+
+create table Cliente(
+	id_cliente int IDENTITY(1,1),
+	id_endereco int,
+	nome Nvarchar(45),
+	email NVarchar(45),
+	telefone NVarchar(20),
+	
+	Constraint pk_cliente primary Key(id_cliente),
+	Constraint fk_cliente_endereco FOREIGN KEY (id_endereco) REFERENCES Endereco(id_endereco)
+
+);
+
+select Cliente.Nome, Cliente.Email, Cliente.Telefone, Endereco.Rua, Endereco.CEP, Endereco.cidade,Endereco.estado 
+from Cliente inner join Endereco 
+on Cliente.id_cliente = Endereco.id_endereco;
+
+
+
+create view Clientes as
+select Cliente.nome, Cliente.Email, Cliente.Telefone, Endereco.Rua, Endereco.CEP, Endereco.cidade,Endereco.estado 
+from Cliente inner join Endereco 
+on Cliente.id_cliente = Endereco.id_endereco
+;
+
